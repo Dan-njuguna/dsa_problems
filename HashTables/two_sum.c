f8f48b8445c94d33f5cb6b322327fa687f8d56f9
Dan-njuguna <njugush020@gmail.com>
2024-08-09 08:53:21 +0300

    Two sum changes to accomodate negative values and larger values

--- HashTables/two_sum.c
+++ HashTables/two_sum.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define HASH_SIZE 10000
 /**
  * twoSum - return the position of values that attain sum of target value.
  * @nums: The array of numbers.
@@ -10,11 +11,10 @@
 */
 int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 {
-    int i, complement, complementIndex;
+    int i, complement, index;
     int *res;
-    int hashSize = 100;
     int *hashTable;
-
+    
     res = (int *)malloc(2 * sizeof(int));
     if (!res)
     {
@@ -22,30 +22,38 @@ int *twoSum(int *nums, int numsSize, int target, int *returnSize)
         fprintf(stderr, "Error allocating memory.\n");
         exit(EXIT_FAILURE);
     }
-    hashTable = (int*)calloc(hashSize, sizeof(int));
+
+    /*Allocate hashTable and initialize to -1*/
+    hashTable = (int *)malloc(HASH_SIZE * sizeof(int));
     if (!hashTable)
     {
         free(res);
         *returnSize = 0;
         exit(EXIT_FAILURE);
     }
+    for (i = 0; i < HASH_SIZE; i++)
+    {
+        hashTable[i] = -1;
+    }
 
     for (i = 0; i < numsSize; i++)
     {
         complement = target - nums[i];
-        complementIndex = hashTable[complement % hashSize];
-        if (complementIndex != 0)
+        index = (complement + 1000) % HASH_SIZE; /*Adjust to handle negative indices*/
+        
+        if (hashTable[index] != -1)
         {
-            res[0] = complementIndex - 1;
+            res[0] = hashTable[index];
             res[1] = i;
             *returnSize = 2;
             free(hashTable);
-            return (res);
+            return res;
         }
-        hashTable[nums[i] % hashSize] = i + 1;
+        
+        index = (nums[i] + 1000) % HASH_SIZE;
+        hashTable[index] = i;
     }
 
-    /*For no valid solution found.*/
     free(hashTable);
     *returnSize = 0;
     return NULL;
@@ -54,8 +62,8 @@ int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 /*Main Test Function*/
 int main(void)
 {
-    int nums[4] = {2, 7, 11, 15};
-    int target = 9;
+    int nums[5] = {3, 2, 95, 4, -3};
+    int target = 92;
     int numsSize = sizeof(nums) / sizeof(nums[0]);
     int returnSize;
     int* result = twoSum(nums, numsSize, target, &returnSize);
