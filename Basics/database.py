0123ec75d9a576626480145f365542f88a41daa9
Dan-njuguna <njugush020@gmail.com>
2024-08-21 09:57:02 +0300

    "Replaced mysql-connector with sqlalchemy, added declarative model for User table, and implemented CRUD operations"

--- Basics/database.py
+++ Basics/database.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import mysql.connector as mc
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -7,15 +6,41 @@ from sqlalchemy.ext.declarative import declarative_base
 details = {
     "user": "root",
     "password": "XXXXXXXX",
-    "host": "localhost",
+    "host": "127.0.0.1",
     "database": "test",
 }
 
-engine = mc.connection.MySQLConnection(**details)
+# Connection string
+connect_string = f"mysql+pymysql://{details['user']}:{details['password']}@{details['host']}/{details['database']}"
 
-if engine and engine.is_connected():
-    print("Connected to MySQL database successfully!")
-else:
-    print("Failed to connect to MySQL database.")
+# Create an engine
+engine = create_engine(connect_string)
+
+# Create a configured "Session" class
+Session = sessionmaker(bind=engine)
+
+# Create a base class for declarsative class definitions
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    fullname = Column(String(50))
+    nickname = Column(String(50))
+
+# Create all tables in the engine
+Base.metadata.create_all(engine)
+
+session = Session()
+
+new_user = User(name="Joe Karanja", fullname="Joe Njoroge Karanja", nickname="Jowie")
+session.add(new_user)
+session.commit()
+
+users = session.query(User).all()
+
+for user in users:
+    print(user.name, user.fullname, user.nickname)
 
 engine.close()
