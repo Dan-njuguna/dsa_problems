cb7efa825532b862da2770f2339cd4db067eb97a
Dan-njuguna <njugush020@gmail.com>
2026-07-05 11:49:12 +0300

    Added: Kata for smallest possible sum(gcd) and setup python for the project to manage envs properly

---
+++ Array_Problems/SmallestSum.go
@@ -0,0 +1,22 @@
+package kata
+
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func SmallestPossibleSum(ar []int) int {
+	if len(ar) == 0 {
+		return 0
+	}
+
+	g := ar[0]
+	for i := 0; i < len(ar); i++ {
+		g = gcd(g, ar[i])
+	}
+
+	return g * len(ar)
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from dsa-problems!")
+
+
+if __name__ == "__main__":
+    main()
